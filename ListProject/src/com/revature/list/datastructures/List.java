0d61945673e958828390ca9ce754867b2c38eff0
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2022-06-23 13:10:08 -0400

    array list

---
+++ ListProject/src/com/revature/list/datastructures/List.java
@@ -0,0 +1,15 @@
+package com.revature.list.datastructures;
+
+public interface List {
+	
+	public void add(Object obj);
+	public Object get (int index);
+	public Object remove (int index);
+	public int indexOf (Object obj);
+	
+	public static void list() {
+		System.out.println("Hello from list interface");
+	}
+}
+
+
